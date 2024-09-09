fcbfc7d621b7f4cd2d19386993e7b51fa8f2573c
宋立君 <github@itjh.com.cn>
2024-09-09 13:09:35 +0800

    Update FindTableViewCell.swift

--- TouTiao/FindTableViewCell.swift
+++ TouTiao/FindTableViewCell.swift
@@ -15,6 +15,7 @@ class FindTableViewCell: UITableViewCell {
  
     @IBOutlet weak var cellImageView: UIImageView!
     @IBOutlet weak var cellImageView1: UIImageView!
+    @IBOutlet weak var cellTextView1: TextView!
 
 
     
